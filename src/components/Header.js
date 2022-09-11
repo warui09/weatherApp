710e331eba33973f7382078e1f96a2644f88fb7c
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-11 18:06:44 +0300

    added css style to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>weather app</h1>
+      <h1
+        style={{
+          fontSize: "50px",
+          fontWeight: "600",
+          color: "grey"
+        }}
+      >
+        weather app
+      </h1>
     </div>
   );
 };
