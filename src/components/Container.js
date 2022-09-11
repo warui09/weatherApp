5c29a70e30201b4059228f5e2bf3b75403053c04
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-11 17:57:25 +0300

    added Header component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import Header from "./Header";
 
 const Container = () => {
   return (
     <div>
+      <Header />
       <input />
       <h2>hello</h2>
     </div>
