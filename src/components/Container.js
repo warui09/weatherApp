dd4e72cee4edf76ef0a35c08d4f8cc70dbd1c4f3
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-12 12:55:31 +0300

    added InputLocation component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
+import InputLocation from "./InputLocation";
 
 const Container = () => {
   return (
     <div>
       <Header />
-      <input />
+      <InputLocation />
       <h2>hello</h2>
     </div>
   );
---
+++ src/components/InputLocation.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+ const InputLocation = () => {
+   return (
+     <div>
+       <form>
+         <input />
+         <button>Submit</button>
+       </form>
+     </div>
+   )
+ }
+ 
+ export default InputLocation
