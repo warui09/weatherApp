7f4ee4c4b9938e545a3d2b97abda5a16ca2c2ec5
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-19 17:28:01 +0300

    added and removed dotenv

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import InputLocation from "./InputLocation";
 import Display from "./Display";
-require('dotenv').config()
 
 const Container = () => {
   const [location, setLocation] = useState("");
@@ -10,11 +9,10 @@ const Container = () => {
   const getLocation = (e) => {
     setLocation(e.target.value);
   };
-  
-  useEffect(() => {
-    console.log('hi')
 
-  })
+  useEffect(() => {
+    console.log("hi");
+  });
 
   return (
     <div>
